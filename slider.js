5f0a9e0d6ae8eea046459ac59d12bf85e60aab02
Burak Gulmez <buraak.gulmez@gmail.com>
2023-01-03 21:15:29 +0300

    first

---
+++ slider.js
@@ -0,0 +1,259 @@
+      var swiper = new Swiper(".mySwiper-header", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+      var swiper = new Swiper('#Head .swiper-container', {
+        nextButton: '#How .swiper-button-next',
+        prevButton: '#How .swiper-button-prev',
+        spaceBetween: 30,
+        loop: true,
+        breakpoints: {
+            1024: {
+                slidesPerView: 1,
+                spaceBetween: 30
+            },
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 30
+            },
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            375: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        },
+        pagination: {
+            el: "#Head .swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: "#Head .swiper-button-next",
+            prevEl: "#Head .swiper-button-prev",
+        }
+    });
+var swiper6 = new Swiper("#How .swiper-container", {
+    nextButton: "#How .swiper-button-next",
+    prevButton: "#How .swiper-button-prev",
+    spaceBetween: 30,
+    loop: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+    pagination: {
+      el: "#How .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: "#How .swiper-button-next",
+      prevEl: "#How .swiper-button-prev",
+    },
+  });
+
+      var swiper = new Swiper("#How2 .swiper-container", {
+        nextButton: "#How2 .swiper-button-next",
+        prevButton: "#How2 .swiper-button-prev",
+        spaceBetween: 30,
+        loop: true,
+        breakpoints: {
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          375: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+        },
+        pagination: {
+          el: "#How2 .swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: "#How2 .swiper-button-next",
+          prevEl: "#How2 .swiper-button-prev",
+        },
+      });
+
+var swiper = new Swiper("#How3 .swiper-container", {
+    nextButton: "#How3 .swiper-button-next",
+    prevButton: "#How3 .swiper-button-prev",
+    spaceBetween: 20,
+    loop: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      676: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+    pagination: {
+      el: "#How3 .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: "#How3 .swiper-button-next",
+      prevEl: "#How3 .swiper-button-prev",
+    },
+  });
+
+var swiper = new Swiper("#Doctor .swiper-container", {
+    nextButton: "#Doctor .swiper-button-next",
+    prevButton: "#Doctor .swiper-button-prev",
+    spaceBetween: 30,
+    loop: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      676: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+    pagination: {
+      el: "#Doctor .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: "#Doctor .swiper-button-next",
+      prevEl: "#Doctor .swiper-button-prev",
+    },
+  });
+
+var swiper = new Swiper("#Smile .swiper-container", {
+    nextButton: "#Smile .swiper-button-next",
+    prevButton: "#Smile .swiper-button-prev",
+    spaceBetween: 50,
+    loop: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+    pagination: {
+      el: "#Smile .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: "#Smile .swiper-button-next",
+      prevEl: "#Smile .swiper-button-prev",
+    },
+  });
+
+var swiper = new Swiper("#Before .swiper-container", {
+    loop: true,
+    loopFillGroupWithBlank: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 230,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: "#Before .swiper-pagitaniton",
+    },
+    navigation: {
+      nextEl: "#Before .swiper-button-next",
+      prevEl: "#Before .swiper-button-prev",
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      676: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+  });
+
+  
